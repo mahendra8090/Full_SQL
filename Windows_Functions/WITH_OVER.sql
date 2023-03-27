b71c13fea1b7575488d701cb235b650127b92965
unknown <mahendra.kumar.min16@itbhu.ac.in>
2023-03-27 21:18:25 +0530

    over function

---
+++ Windows_Functions/WITH_OVER.sql
@@ -0,0 +1,15 @@
+use AdventureWorks2019;
+
+-- Use of aggregate function with over
+select 
+P.FirstName,
+P.LastName,
+E.JobTitle,
+EPH.Rate,
+AVG(EPH.Rate) over() As AverageRate,
+Max(EPH.Rate) over() As MaximumRate,
+EPH.Rate-AVG(EPH.Rate) over() As DiffFromAvgRate,
+EPH.Rate/Max(EPH.Rate) over() As PercentOfMaxRate
+from Person.Person P
+inner join HumanResources.Employee E on P.BusinessEntityID=E.BusinessEntityID
+inner join HumanResources.EmployeePayHistory EPH on EPH.BusinessEntityID=P.BusinessEntityID
