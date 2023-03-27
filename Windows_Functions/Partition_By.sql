6343c7436d4b7f5e9d737ed8ab27958f0e0c73d0
unknown <mahendra.kumar.min16@itbhu.ac.in>
2023-03-27 21:35:30 +0530

    Partition function

---
+++ Windows_Functions/Partition_By.sql
@@ -0,0 +1,13 @@
+use AdventureWorks2019;
+
+select 
+P.Name as ProuctName,
+P.ListPrice,
+PS.Name As ProductSubcategory,
+PC.Name As ProuctCategory,
+avg(P.ListPrice) over(Partition by PC.Name) AvgPriceByCategory,
+avg(P.ListPrice) over(Partition by PC.name,PS.Name) AvgPriceByCategoryAndSubCategory,
+P.ListPrice-avg(P.ListPrice) over(Partition by PC.Name) ProductVsCategoryDelta
+from Production.Product P
+inner join Production.ProductSubcategory PS on PS.ProductSubcategoryID=P.ProductSubcategoryID
+inner join Production.ProductCategory PC on PS.ProductCategoryID=PC.ProductCategoryID
